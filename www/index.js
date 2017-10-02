0375a29ac6e4ea30727dacd422910ed1df8864e3
Korawit <korawit.p@gmail.com>
2017-10-02 18:13:44 +0700

    Take photo

--- www/index.js
+++ www/index.js
@@ -2,11 +2,25 @@ ons.ready(function() {
     initTimeline();
     var tabar = document.querySelector('ons-tabbar');
     tabar.addEventListener('postchange', function(event) {
-
         if(event.index == 0){
             initTimeline(event);
         }
+    });
 
+    $('#takephoto').click(function(){
+        console.log("Take a photo");
+        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI });
+        
+        function onSuccess(imageURI) {
+            console.log(imageURI);
+            var image =  $("#preview");
+            image.attr("src",imageURI);
+        }
+        
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
     });
 });
 
@@ -23,3 +37,5 @@ function initTimeline(event){
         });
     });
 }
+
+
